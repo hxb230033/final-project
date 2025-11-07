1f6fe8eaa4dc0504f02bce1b49616797a85f6622
Hailey Browning <hxb230033@utdallas.edu>
2025-11-07 10:40:11 -0600

    Added player movement and sprite.

--- src/project.py
+++ src/project.py
@@ -1,25 +1,51 @@
 import pygame
-import time
 import random
+import time
+
 
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sandwich Stacker")
 BG = pygame.image.load("background.png")
+clock = pygame.time.Clock()
 
-def draw():
-    WIN.blit(BG, (0,0))
-    pygame.display.update()
+class Player(pygame.sprite.Sprite):
+    def __init__(self, image_path, x, y):
+        super().__init__()
+        self.image = pygame.image.load(image_path)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.speed = 5
+
+    def move(self,keys):
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
 
 def main():
-    run = True
+    pygame.init()
 
+    player_sprite = Player("player.png", 500, 425)
+
+    run = True
     while run:
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-        draw()
+    
+        keys = pygame.key.get_pressed()
+        player_sprite.move(keys)
+
+        WIN.blit(BG, (0,0))
+        player_sprite.draw(WIN)
+        pygame.display.flip()
     pygame.quit()
 
 if __name__ == "__main__":
